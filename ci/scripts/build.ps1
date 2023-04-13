af4e4af40b8d127724202e2e7c1655729cb2b959
Sean OMeara <sean@sean.io>
2023-04-13 10:47:00 +0200

    tweaking

--- ci/scripts/build.ps1
+++ ci/scripts/build.ps1
@@ -1,4 +1,6 @@
-msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Debug /property:Platform=Win32
-msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Release /property:Platform=Win32
-msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Debug /property:Platform=x64
-msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Release /property:Platform=x64
+# msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Debug /property:Platform=Win32
+# msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Release /property:Platform=Win32
+# msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Debug /property:Platform=x64
+# msbuild windows\\ZeroTierOne\\ZeroTierOne.vcxproj /m /p:Configuration=Release /property:Platform=x64
+
+msbuild windows\\ZeroTierOne.sln /m /p:Configuration=Release /property:Platform=x64 /t:ZeroTierOne:Rebuild
